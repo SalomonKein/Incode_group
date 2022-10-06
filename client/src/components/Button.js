13a7ef3985b8edcb92cb6bbccde1d04e64bce1cf
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2022-10-06 21:53:31 +0300

    first commit

---
+++ client/src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.2rem 1rem;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  background: ${({background}) => background || '#4682B4'};
+  &:hover {
+    background: #afeeee;
+    color: black;
+  }
+`;
+
+export default function Button(props) {
+  return <StyledButton {...props} />;
+}
